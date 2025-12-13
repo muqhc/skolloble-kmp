209d02d6e5f8a37027c2c52f04f54d171c00f2b6
muqhc <muqhc07@gmail.com>
2025-12-13 23:39:14 +0900

    apply antlr

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Skolloble KMP
 
 [Skolloble](https://github.com/muqhc/skolloble) for Kotlin Multiplatform
+
+## Note
+
+depends on [Strumenta/antlr-kotlin](https://github.com/Strumenta/antlr-kotlin)
+
--- skolloble-parser-kmp/build.gradle.kts
+++ skolloble-parser-kmp/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.strumenta.antlrkotlin.gradle.AntlrKotlinTask
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.strumenta.antlrKotlin)
 }
 
 group = "io.github.muqhc"
@@ -11,6 +13,7 @@ version = "1.0.0"
 kotlin {
     jvm()
 
+    mingwX64()
     linuxX64()
 
     js {
@@ -25,14 +28,38 @@ kotlin {
     }
 
     sourceSets {
-        commonMain.dependencies {
-            //put your multiplatform dependencies here
+        commonMain {
+            dependencies {
+                implementation("com.strumenta:antlr-kotlin-runtime:${libs.versions.strumentaAntlrKotlin.get()}")
+            }
         }
 
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
     }
+}
+
+val generateKotlinGrammarSource = tasks.register<AntlrKotlinTask>("generateKotlinGrammarSource") {
+    dependsOn("cleanGenerateKotlinGrammarSource")
+
+    // ANTLR .g4 files are under {example-project}/antlr
+    // Only include *.g4 files. This allows tools (e.g., IDE plugins)
+    // to generate temporary files inside the base path
+    source = fileTree(layout.projectDirectory.dir("grammar/antlr")) {
+        include("**/*.g4")
+    }
+
+    // We want the generated source files to have this package name
+    val pkgName = "io.github.muqhc.skolloblekmp.parser.generated.antlr"
+    packageName = pkgName
+
+    // We want visitors alongside listeners.
+    // The Kotlin target language is implicit, as is the file encoding (UTF-8)
+    arguments = listOf("-visitor")
+
+    val outDir = "src/commonMain/kotlin/${pkgName.replace(".", "/")}"
+    outputDirectory =  layout.projectDirectory.dir(outDir).asFile
 }
 
 mavenPublishing {
@@ -66,3 +93,5 @@ mavenPublishing {
         }
     }
 }
+
+
